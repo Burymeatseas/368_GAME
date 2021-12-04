5f804dd17f85a251a3ba35c61dbb67f7a0b33e56
Kit Noorani <Christopher.noorani.153@my.csun.edu>
2021-12-04 12:31:08 -0800

    Were makin progress

--- HorrorGame_368/Assets/Scripts/Crate/Shatter.cs
+++ HorrorGame_368/Assets/Scripts/Crate/Shatter.cs
@@ -1,16 +1,32 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shatter : MonoBehaviour
 {
 	public GameObject shatteredVersion;
+	
+	public float MaxHealth =  50;
+	
+	float currentHealth;
+	
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(shatteredVersion, transform.position, transform.rotation);
-		Destroy(gameObject);
+        currentHealth = MaxHealth;
     }
-
-   
+	
+	public void ApplyDamage(float damage)
+	{
+		currentHealth -= damage;
+		
+		if(currentHealth < 0)Break();
+	}
+	
+	void Break()
+	{
+		Instantiate(shatteredVersion, transform.position, transform.rotation);
+		 
+		Destroy(gameObject);
+	}
 }
--- HorrorGame_368/Assets/Scripts/Maps/ImageLevelGeneration.cs
+++ HorrorGame_368/Assets/Scripts/Maps/ImageLevelGeneration.cs
@@ -14,7 +14,7 @@ public class ImageLevelGeneration : MonoBehaviour
         Color[] pixels = levelImage.GetPixels();
 		
 		int worldX = levelImage.width;
-		int worldY = levelImage.height;
+		int worldZ = levelImage.height;
 		
 		Vector3[] spawnPositions =
 			new Vector3[pixels.Length];
@@ -22,12 +22,56 @@ public class ImageLevelGeneration : MonoBehaviour
 		Vector3 startingSpawnPosition =
 			new Vector3(-Mathf.Round(worldX / 2),
 						0,
-						-Mathf.Round(worldY / 2));
+						-Mathf.Round(worldZ / 2));
 		
 		Vector3 currentSpawnPosition =
 			startingSpawnPosition;
 			
 			
+			
+			
+		int counter = 0;
+		for (int z = 0; z < worldZ; z++)
+		{
+			for (int x = 0; z < worldX; x++)
+			{
+				spawnPositions[counter] =
+					currentSpawnPosition;
+				counter++;
+				currentSpawnPosition.x++;
+			}
+			
+			currentSpawnPosition.x =
+				startingSpawnPosition.x;
+			currentSpawnPosition.z++;
+		}
+		counter = 0;
+		
+		
+		
+		foreach (Vector3 pos in spawnPositions)
+		{
+			Color c = pixels[counter];
+			
+			if (c.Equals(Color.black))
+			{
+				var newGround =
+					Instantiate(groundPrefab);
+					
+				newGround.transform.position =
+				pos;
+			}
+			
+			else if (c.Equals(Color.white))
+			{
+				var newWall =
+					Instantiate(wallPrefab);
+					
+				newWall.transform.position =
+				pos;
+			}
+			counter++;
+		}
     }
 
   
